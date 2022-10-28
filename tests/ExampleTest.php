711ba42ec07d7ec4c5edcf14b279c0e01956bbf7
Stefano Novelli <murdercode@gmail.com>
2022-10-28 15:04:10 +0200

    commit wip

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,9 @@
 <?php
 
-it('can test', function () {
-    expect(true)->toBeTrue();
+use Murdercode\LaravelShortcodePlus\LaravelShortcodePlus;
+
+it('can parse twitter shortcode', function() {
+    $html = "[twitter url=\"https://twitter.com/elonmusk/status/1585841080431321088\"]";
+    $twitterOembed = LaravelShortcodePlus::source($html)->parseTwitterTag();
+    expect($twitterOembed)->toContain('<blockquote class="twitter-tweet" data-conversation="none" data-dnt="true" data-theme="light">');
 });
