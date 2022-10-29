2bab66ed502b5debff34817d6cf919fc90ca10ac
Stefano Novelli <murdercode@gmail.com>
2022-10-29 17:41:36 +0200

    wip

--- resources/views/youtube.blade.php
+++ resources/views/youtube.blade.php
@@ -1,7 +1,7 @@
-@if($video_id)
-<span class="__shortcode_youtube">
-    <iframe src="https://www.youtube-nocookie.com/embed/{{ $video_id }}&autoplay=1" srcdoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube-nocookie.com/embed/{{ $video_id }}?autoplay=1><img style='object-fit:cover;height:100%;' loading='lazy' src=https://img.youtube.com/vi/{{ $video_id }}/hqdefault.jpg alt='{{ $video_id }}'
+@if($youtubeId)
+	<span class="__shortcode_youtube">
+    <iframe src="https://www.youtube-nocookie.com/embed/{{ $youtubeId }}&autoplay=1" srcdoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube-nocookie.com/embed/{{ $youtubeId }}?autoplay=1><img style='object-fit:cover;height:100%;' loading='lazy' src=https://img.youtube.com/vi/{{ $youtubeId }}/hqdefault.jpg alt='{{ $youtubeId }}'
         loading=lazy><span>▶</span></a>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope;
-        picture-in-picture" allowfullscreen title="{{ $video_id }}"></iframe>
+        picture-in-picture" allowfullscreen title="{{ $youtubeId }}"></iframe>
 </span>
 @endif
--- src/Parsers/Youtube.php
+++ src/Parsers/Youtube.php
@@ -7,22 +7,16 @@ class Youtube
     public static function parse(string $content): string
     {
         preg_match('/\[youtube url="(.*?)"]/', $content, $matches);
-        $youtubeId = $matches[1] ?? null;
-        if ($youtubeId) {
-            $youtubeId = explode('v=', $youtubeId)[1];
-            $youtubeId = explode('&', $youtubeId)[0];
-            return str_replace(
-                $matches[0],
-                view('shortcode-plus::youtube', ['video_id' => $youtubeId])->render(),
-                $content
-            );
-        }
+        $url = $matches[1] ?? null;
 
-        return preg_replace(
-            '/\[youtube]/',
-            'No youtube URL defined',
-            $content
-        );
+        $youtubeId = preg_match('/youtube.(.*)watch\?v=([a-zA-Z0-9_-]{11})/', $url, $matches)
+            ? $matches[2]
+            : null;
+
+        return $youtubeId
+            ? view('shortcode-plus::youtube', ['youtubeId' => $youtubeId])
+                ->render()
+            : 'No youtube URL defined';
     }
 
 }
