30fcd88fa86aa773ead623f3823d6427b8730b3d
Stefano Novelli <murdercode@gmail.com>
2022-10-30 09:58:34 +0100

    Fix missing spotify control

--- src/Parsers/Spotify.php
+++ src/Parsers/Spotify.php
@@ -9,11 +9,18 @@ class Spotify
     {
         return preg_replace_callback('/\[spotify uri="(.+)"]/', function ($matches) {
             $url = $matches[1] ?? null;
-            $url = str_replace('spotify:album:', 'https://open.spotify.com/embed/album/', $url);
+            $url = str_contains($url, 'spotify') ? $url : null;
+            $url = $url ? str_replace(
+                'spotify:album:',
+                'https://open.spotify.com/embed/album/',
+                $url
+            ) : null;
 
             if ($url) {
                 return view('shortcode-plus::spotify', ['url' => $url]);
             }
+
+            return 'No spotify URI defined';
         }, $content);
     }
 
