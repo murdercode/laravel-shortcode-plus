c695bd8a61d1c23aa93607dbadbb3f5ac7e83ab5
Stefano Novelli <murdercode@gmail.com>
2023-02-13 17:33:26 +0100

    Merge new version

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -14,10 +14,13 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Murdercode\\LaravelShortcodePlus\\Database\\Factories\\'.class_basename($modelName).'Factory'
+            fn(string $modelName
+            ) => 'Murdercode\\LaravelShortcodePlus\\Database\\Factories\\' . class_basename(
+                    $modelName
+                ) . 'Factory'
         );
 
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
     }
 
     protected function getPackageProviders($app)
