e0549a5fbce705ebf8814c538a9d8b18e748437f
Stefano Novelli <murdercode@gmail.com>
2023-03-17 15:37:12 +0100

    Add trailing slash to storage

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -1,44 +1,39 @@
 <div class="w-full">
-	<div class="grid grid-cols-4 grid-rows-2 not-prose sm:px-0">
-		@foreach ($images as $image)
+    <div class="grid grid-cols-4 grid-rows-2 not-prose sm:px-0">
+        @foreach ($images as $image)
+            @if ($loop->iteration <= 5)
+                <a class="
 
-			@if($loop->iteration <= 5)
-				<a class="
-
-			@if($loop->count > 2 && $loop->iteration == 1) col-span-2 row-span-2 @endif
-			@if($loop->count == 2 && ($loop->iteration == 1 || $loop->iteration == 2)) col-span-2 row-span-2 @endif
-			@if($loop->count == 2 && $loop->iteration == 2) col-span-2 @endif
-			@if($loop->count == 3 && ($loop->iteration == 2 || $loop->iteration == 3)) col-span-2 @endif
-			@if($loop->count == 4 && $loop->iteration == 4) col-span-2 @endif
+			@if ($loop->count > 2 && $loop->iteration == 1) col-span-2 row-span-2 @endif
+			@if ($loop->count == 2 && ($loop->iteration == 1 || $loop->iteration == 2)) col-span-2 row-span-2 @endif
+			@if ($loop->count == 2 && $loop->iteration == 2) col-span-2 @endif
+			@if ($loop->count == 3 && ($loop->iteration == 2 || $loop->iteration == 3)) col-span-2 @endif
+			@if ($loop->count == 4 && $loop->iteration == 4) col-span-2 @endif
 
 			glightbox hover:brightness-110 relative"
-				   href="{{ asset('storage' . $image["path"]) }}"
-				   data-glightbox="{{ addslashes($image['title']) }}">
-					<img
-							class="cursor-pointer relative object-cover w-full h-full"
-							src="{{ asset('storage' . $image["path"]) }}"
-							alt="{{ $image["alternative_text"] }}"
-							title="Clicca per vedere l'immagine originale"/>
-					{{--Hover Count--}}
-					@if($loop->iteration == 5 && $loop->count > 5)
-						<div class="absolute inset-0 text-white flex items-center justify-center bg-red-500/50 font-black">
-							+{{$loop->count - 5}}
-						</div>
-					@endif
+                    href="{{ asset('storage/' . $image['path']) }}" data-glightbox="{{ addslashes($image['title']) }}">
+                    <img class="relative object-cover w-full h-full cursor-pointer"
+                        src="{{ asset('storage/' . $image['path']) }}" alt="{{ $image['alternative_text'] }}"
+                        title="Clicca per vedere l'immagine originale" />
+                    {{-- Hover Count --}}
+                    @if ($loop->iteration == 5 && $loop->count > 5)
+                        <div
+                            class="absolute inset-0 flex items-center justify-center font-black text-white bg-red-500/50">
+                            +{{ $loop->count - 5 }}
+                        </div>
+                    @endif
 
-				</a>
+                </a>
 
-				{{--Hidden images--}}
-			@else($loop->iteration > 5)
-				<a href="{{ asset('storage' . $image["path"]) }}"
-				   class="hidden"></a>
-			@endif
-
-		@endforeach
-	</div>
-	@if($title)
-		<div class="font-sans text-base bg-gray-200 dark:bg-zinc-800 px-2 py-1.5">
-			{{ $title }}
-		</div>
-	@endif
+                {{-- Hidden images --}}
+            @else($loop->iteration > 5)
+                <a href="{{ asset('storage' . $image['path']) }}" class="hidden"></a>
+            @endif
+        @endforeach
+    </div>
+    @if ($title)
+        <div class="font-sans text-base bg-gray-200 dark:bg-zinc-800 px-2 py-1.5">
+            {{ $title }}
+        </div>
+    @endif
 </div>
--- resources/views/image.blade.php
+++ resources/views/image.blade.php
@@ -1,18 +1,16 @@
 <div class="relative block">
     <span
-		    class="block absolute right-0 px-1 py-0.5 text-xs text-gray-600 bg-white not-prose dark:bg-gray-900 dark:text-gray-300 opacity-60">
+        class="block absolute right-0 px-1 py-0.5 text-xs text-gray-600 bg-white not-prose dark:bg-gray-900 dark:text-gray-300 opacity-60">
         {{ $credits }}
     </span>
-	<figure>
-		<a href="{{ asset('storage' . $path) }}" class="glightbox"
-		   data-glightbox="{{ addslashes($title) }}">
-			<img class="mx-auto cursor-pointer" src="{{ asset('storage' . $path) }}"
-			     alt="{{ $alternative_text }}"
-			     title="Clicca per vedere l'immagine originale" loading="lazy" width="{{ $width }}"
-			     height="{{ $height }}"/>
-		</a>
-		<figcaption class="bg-gray-200 dark:bg-zinc-800 px-2 py-1.5 font-sans text-base">
-			{!! $caption !!}
-		</figcaption>
-	</figure>
+    <figure>
+        <a href="{{ asset('storage/' . $path) }}" class="glightbox" data-glightbox="{{ addslashes($title) }}">
+            <img class="mx-auto cursor-pointer" src="{{ asset('storage/' . $path) }}" alt="{{ $alternative_text }}"
+                title="Clicca per vedere l'immagine originale" loading="lazy" width="{{ $width }}"
+                height="{{ $height }}" />
+        </a>
+        <figcaption class="bg-gray-200 dark:bg-zinc-800 px-2 py-1.5 font-sans text-base">
+            {!! $caption !!}
+        </figcaption>
+    </figure>
 </div>
