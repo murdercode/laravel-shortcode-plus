dc7f4c28eabc21676fb24bc9f3219bf4fc473a82
Stefano Novelli <murdercode@gmail.com>
2023-03-18 18:32:44 +0100

    Remove bg figcaption images

--- resources/views/image.blade.php
+++ resources/views/image.blade.php
@@ -1,20 +1,20 @@
 <div class="relative block">
-	@if($credits)
-    <span
-        class="block absolute right-0 px-1 py-0.5 text-xs text-gray-600 bg-white not-prose dark:bg-gray-900 dark:text-gray-300 opacity-60">
-        {{ $credits }}
-    </span>
-	@endif
+    @if ($credits)
+        <span
+            class="block absolute right-0 px-1 py-0.5 text-xs text-gray-600 bg-white not-prose dark:bg-gray-900 dark:text-gray-300 opacity-60">
+            {{ $credits }}
+        </span>
+    @endif
     <figure>
         {{-- <a href="{{ asset('storage/' . $path) }}" class="glightbox" data-glightbox="{{ addslashes($title) }}"> --}}
-            <img class="mx-auto cursor-pointer" src="{{ asset('storage/' . $path) }}" alt="{{ $alternative_text }}"
-                title="Clicca per vedere l'immagine originale" loading="lazy" width="{{ $width }}"
-                height="{{ $height }}" />
+        <img class="mx-auto cursor-pointer" src="{{ asset('storage/' . $path) }}" alt="{{ $alternative_text }}"
+            title="Clicca per vedere l'immagine originale" loading="lazy" width="{{ $width }}"
+            height="{{ $height }}" />
         {{-- </a> --}}
-		  @if($caption)
-        <figcaption class="bg-gray-200 dark:bg-zinc-800 px-2 py-1.5 font-sans text-base">
-            {!! $caption !!}
-        </figcaption>
-		  @endif
+        @if ($caption)
+            <figcaption class="px-2 py-1.5 font-sans text-base">
+                {!! $caption !!}
+            </figcaption>
+        @endif
     </figure>
 </div>
