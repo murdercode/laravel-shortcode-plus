59a9f8363f923785f962c5c95c10ed1d32a38ba8
Stefano Novelli <murdercode@gmail.com>
2023-05-04 11:00:28 +0200

    Distico support

--- resources/views/distico.blade.php
+++ resources/views/distico.blade.php
@@ -1,4 +1,4 @@
 <div class="shortcode_distico">
-	<div class="shortcode_distico_container">{!! $content !!}</div>
 	<div class="shortcode_distico_border"></div>
+	<div class="shortcode_distico_container">{!! $content !!}</div>
 </div>
