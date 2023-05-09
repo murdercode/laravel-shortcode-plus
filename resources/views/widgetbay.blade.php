2fde80c7474b259257d964048d933177fe498798
Stefano Novelli <murdercode@gmail.com>
2023-05-09 16:51:33 +0200

    Support for Widgetbay

--- resources/views/widgetbay.blade.php
+++ resources/views/widgetbay.blade.php
@@ -1,2 +1,2 @@
-<iframe src="https://widgetbay.3labs.it/widgetbox/{{$id}}" allowfullscreen="true"
-        class="_iub_cs_activate" frameborder="0" height="229" loading="lazy" style="width:100%"></iframe>
+<iframe src="https://widgetbay.3labs.it/widgetbox/{{$id}}" allowfullscreen="true" scrolling="no"
+        class="shortcode_widgetbay _iub_cs_activate" frameborder="0" height="340" loading="lazy" style="width:100%;overflow-y:hidden;"></iframe>
