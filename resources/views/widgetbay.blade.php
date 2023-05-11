583875c4822de7451279e7cf367c659d6b29af32
Stefano Novelli <murdercode@gmail.com>
2023-05-11 10:34:04 +0200

    Highest shortcode widgetbay

--- resources/views/widgetbay.blade.php
+++ resources/views/widgetbay.blade.php
@@ -1,2 +1,2 @@
 <iframe src="https://widgetbay.3labs.it/widgetbox/{{$id}}" allowfullscreen="true" scrolling="no"
-        class="shortcode_widgetbay _iub_cs_activate" frameborder="0" height="340" loading="lazy" style="width:100%;overflow-y:hidden;"></iframe>
+        class="shortcode_widgetbay _iub_cs_activate" frameborder="0" height="420" loading="lazy" style="width:100%;overflow-y:hidden;"></iframe>
