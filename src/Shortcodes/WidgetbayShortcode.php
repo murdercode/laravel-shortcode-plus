fe8c6c5bc7fdbd759114cf5575fd502bc85a0551
Stefano Novelli <murdercode@gmail.com>
2023-06-04 14:50:02 +0200

    Widgetbay id check and if it is numeric

--- src/Shortcodes/WidgetbayShortcode.php
+++ src/Shortcodes/WidgetbayShortcode.php
@@ -6,7 +6,11 @@ class WidgetbayShortcode
 {
     public function register($shortcode)
     {
-        $id = $shortcode->id ?? '';
+        if (! isset($shortcode->id) || ! is_numeric($shortcode->id)) {
+            return 'No Widgetbay parameter id defined';
+        }
+
+        $id = $shortcode->id;
 
         return view('shortcode-plus::widgetbay', compact('id'))->render();
     }
