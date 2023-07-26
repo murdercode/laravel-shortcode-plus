336fe0993150e91df19cf8f2cf7e6cb0377db145
Stefano Novelli <murdercode@gmail.com>
2023-07-26 17:47:11 +0200

    Better article check model

--- src/Shortcodes/LeggiancheShortcode.php
+++ src/Shortcodes/LeggiancheShortcode.php
@@ -9,11 +9,12 @@ class LeggiancheShortcode
 
         $id = preg_match('/\d+/', $shortcode->get('id'), $matches) ? $matches[0] : null;
 
-        // If App\Models\Article is not found, try \App\Models\Post
         if (class_exists('\App\Models\Article')) {
-            $article = Article::find($id);
+            $article = \App\Models\Article::find($id);
         } elseif (class_exists('\App\Models\Post')) {
-            $article = Post::find($id);
+            $article = \App\Models\Post::find($id);
+        } else {
+            $article = null;
         }
 
         if (! isset($article)) {
