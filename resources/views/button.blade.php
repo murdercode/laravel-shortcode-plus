cb5c90579d4f1f6b70f219128d27f770751da6a9
Stefano Novelli <murdercode@gmail.com>
2023-07-28 14:32:05 +0200

    Remove noopener on button

--- resources/views/button.blade.php
+++ resources/views/button.blade.php
@@ -1,4 +1,4 @@
-<a class="shortcode_button {{$level}}" href="{{$link}}" target="_blank" rel="noopener"
+<a class="shortcode_button {{$level}}" href="{{$link}}" target="_blank"
    @if($isSponsored) rel="nofollow noopener sponsored" @endif>
     {{$label}}
 </a>
