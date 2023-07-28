474d3c827e60d051ab95120dc37470e686acdc74
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-07-28 11:13:33 +0200

    Fix config file

--- config/shortcode-plus.php
+++ config/shortcode-plus.php
@@ -19,8 +19,15 @@ return [
     'tmdb' => [
         'api_key' => env('TMDB_API_KEY'),
         'api_version' => '3',
+        'language' => 'it',
         'base_uri' => 'https://api.themoviedb.org',
         'image_base_uri' => 'https://image.tmdb.org/t/p',
-        'more_link' => 'https://www.themoviedb.org',
+        'more_link' => [
+            'domain' => 'https://example.com',
+            'movie-prefix' => 'movies',
+            'tv-prefix' => 'tvs'
+        ],
+        'cache_ttl' => 60 * 60 * 24, // 1 day
+        'justwatch_api_key' => env('JUSTWATCH_API_KEY'),
     ],
 ];
