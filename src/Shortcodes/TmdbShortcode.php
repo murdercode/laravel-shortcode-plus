5e9544526ab43b0151ab56f93f66cdebe857b824
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-07-28 11:34:00 +0200

    Fix TMDB shortcode

--- src/Shortcodes/TmdbShortcode.php
+++ src/Shortcodes/TmdbShortcode.php
@@ -24,9 +24,20 @@ class TmdbShortcode
         }
 
         $data = $this->getTmdbDataFromApi($type, $id);
+        $moreLink = $this->generateMoreLink($type, $id);
 
         return view('shortcode-plus::tmdb', compact('data'))->render();
 
+    }
+
+    /**
+     * Get TMDB item
+     */
+    public function getTmdbData($type, $id)
+    {
+        return cache("laravel-shortcode-plus::tmdb.$type.$id", function () use ($type, $id) {
+            return $this->getTmdbDataFromApi($type, $id);
+        }, config('shortcode-plus.tmdb.cache_ttl'));
     }
 
     /**
@@ -36,18 +47,46 @@ class TmdbShortcode
     {
         $tmdbApiVersion = config('shortcode-plus.tmdb.api_version');
         $tmdbApiKey = config('shortcode-plus.tmdb.api_key');
+        $tmdbLanguage = config('shortcode-plus.tmdb.language');
 
         $response = Http::withHeaders([
             'Authorization' => "Bearer $tmdbApiKey",
             'accept' => 'application/json',
         ])
             ->acceptJson()
-            ->get("https://api.themoviedb.org/$tmdbApiVersion/$type/$id");
+            ->get("https://api.themoviedb.org/$tmdbApiVersion/$type/$id?language=$tmdbLanguage");
+
+
 
         if ($response->failed()) {
             throw new \Exception('Error while fetching data from TMDB API');
         }
 
         return json_decode($response->body());
+    }
+
+    /**
+     * Generate More Link
+     */
+    public function generateMoreLink($type, $id) : ?string
+    {
+        $moreLinkDomain = config('shortcode-plus.tmdb.more_link.domain');
+
+        if(!$moreLinkDomain) {
+            return null;
+        }
+
+        $moreLinkType = match($type) {
+            'movie' => config('shortcode-plus.tmdb.more_link.movie-prefix'),
+            'tv' => config('shortcode-plus.tmdb.more_link.tv-prefix'),
+            default => null,
+        };
+
+        if(!$moreLinkType) {
+            return null;
+        }
+
+        return "$moreLinkDomain/$moreLinkType/$id";
+
     }
 }
