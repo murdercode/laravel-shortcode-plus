8b7a9a24b7a8336e05f5d8cda9eb14488254ee0d
Stefano Novelli <murdercode@gmail.com>
2023-07-28 13:54:26 +0200

    Add Tmdb tests

--- tests/TmdbShortcodeTest.php
+++ tests/TmdbShortcodeTest.php
@@ -7,6 +7,18 @@ it('can render tmdb button', function () {
     $html = '[tmdb type="movie" id="tt0111161"]';
     $parsedHtml = LaravelShortcodePlus::source($html)->parseAll();
 
-    dd($parsedHtml);
+    expect($parsedHtml)
+        ->toContain('img src="https://image.tmdb.org/t/p/w154/9OxcvTJwZDjQTFvY2NxiwnSrQS6.jpg"')
+        ->toContain('Le ali della libertà (1994)')
+        ->toContain('<a href="https://example.com/movies/tt0111161"');
+})->skip();
 
-});
+it('can render tmdb button with tv type', function () {
+    $html = '[tmdb type="tv" id="2004"]';
+    $parsedHtml = LaravelShortcodePlus::source($html)->parseAll();
+
+    expect($parsedHtml)
+        ->toContain('https://image.tmdb.org/t/p/w154/kkEyQQro7M0Avxfiio81alDPdTd.jpg"')
+        ->toContain('Malcolm (2000)')
+        ->toContain('<a href="https://example.com/tvs/2004"');
+})->skip();
