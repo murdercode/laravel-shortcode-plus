636be838a4144356d3e4e9cca776e240f8430887
Stefano Novelli <murdercode@gmail.com>
2023-08-10 11:43:46 +0200

    Fix Tiktok test

--- tests/TikTokShortcodeTest.php
+++ tests/TikTokShortcodeTest.php
@@ -21,7 +21,7 @@ it('can parse tiktok shortcode if the url is not defined ', function () {
 });
 
 it('cannot get tiktok oembed', function () {
-    $html = '[twitter url="https://www.tiktok.com/@tomshardwareita/video/72643?lang=it-IT"]';
+    $html = '[tiktok url="https://www.tiktok.com/@tomshardwareita/video/72643?lang=it-IT"]';
     $tiktokOembed = LaravelShortcodePlus::source($html)->parseAll();
     expect($tiktokOembed)->toContain('Cannot get TikTok oembed');
 });
