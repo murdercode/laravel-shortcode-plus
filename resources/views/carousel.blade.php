d84725fe049ee80aea18d110f508ea28c435b8ff
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-08-23 12:41:31 +0200

    Add glightbox on splide carousel

--- resources/views/carousel.blade.php
+++ resources/views/carousel.blade.php
@@ -1,9 +1,15 @@
-<section class="splide" aria-label="Didascalia">
+<section class="splide">
     <div class="splide__track">
         <ul class="splide__list">
             @foreach (json_decode($images) as $image)
                 <li class="splide__slide">
-                    <img src="{{ asset('storage/' . $image->src )}}" class="w-full h-full object-cover" loading="lazy">
+                    <img src="{{ asset('storage/' . $image->src )}}"
+                         alt="{{ $image->title }}"
+                         class="w-full h-full object-cover glightbox"
+                         data-glightbox="{{ addslashes('carousel-' . $image->title) }}"
+                         loading="lazy"
+                         title="Clicca per vedere l'immagine originale
+                    ">
                 </li>
             @endforeach
         </ul>
