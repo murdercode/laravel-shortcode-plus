bbd612d1f69be526a0137ccaa67dfb2f2692b50d
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-08-23 11:26:32 +0200

    Add GLightbox

--- resources/src/js/shortcode.js
+++ resources/src/js/shortcode.js
@@ -1,8 +1,13 @@
 import Splide from "@splidejs/splide";
+import GLightbox from 'glightbox';
 
+// Splide
 let splide = new Splide( '.splide', {
     type   : 'loop',
     padding: '5rem',
 } );
 
 splide.mount();
+
+// GLightbox
+const lightbox = GLightbox();
--- resources/views/carousel.blade.php
+++ resources/views/carousel.blade.php
@@ -14,6 +14,7 @@
     </div>
 </section>
 
+
 {{--<script>--}}
 {{--    let splide = new Splide( '.splide', {--}}
 {{--        type   : 'loop',--}}
