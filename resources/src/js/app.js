85c01e7bd8c0c64e5adebd78510f0dc8b146db75
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-08-29 10:16:43 +0200

    Add DOMContentLoaded

--- resources/src/js/app.js
+++ resources/src/js/app.js
@@ -2,28 +2,32 @@ import Splide from "@splidejs/splide";
 import GLightbox from 'glightbox';
 import ImageCompare from "image-compare-viewer";
 
+document.addEventListener( 'DOMContentLoaded', function() {
+
 // Splide
-const checkSpide = document.querySelector(".splide");
-if (checkSpide !== null) {
+    const checkSpide = document.querySelector(".splide");
+    if (checkSpide !== null) {
 
-    let splide = new Splide('.splide', {
-        type: 'loop',
-        padding: '5rem',
-    });
+        let splide = new Splide('.splide', {
+            type: 'loop',
+            padding: '5rem',
+        });
 
-    splide.mount();
-}
+        splide.mount();
+    }
 
 // GLightbox
-const checkLightbox = document.querySelector(".glightbox");
+    const checkLightbox = document.querySelector(".glightbox");
 
-if (checkLightbox !== null) {
-    const lightbox = GLightbox();
-}
+    if (checkLightbox !== null) {
+        const lightbox = GLightbox();
+    }
 // Image Compare
-const viewers = document.querySelectorAll(".image-compare");
-if(viewers !== null){
-    viewers.forEach((element) => {
-        let view = new ImageCompare(element).mount();
-    });
-}
+    const viewers = document.querySelectorAll(".image-compare");
+    if (viewers !== null) {
+        viewers.forEach((element) => {
+            let view = new ImageCompare(element).mount();
+        });
+    }
+
+}, false );
