6cc442c9140ef05dfae8dfc39872ee525045d633
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-10-12 16:09:38 +0200

    Fix twitter style

--- resources/views/twitter.blade.php
+++ resources/views/twitter.blade.php
@@ -1,7 +1,7 @@
 @if($html)
-	<div class="flex justify-center">
-	{!! $html !!}
-	</div>
+	<div class="flex justify-center twitter-card" style="height: 764px">
+	    {!! $html !!}
+    </div>
 @else
 	<p>Sorry, no tweets found.</p>
 @endif
