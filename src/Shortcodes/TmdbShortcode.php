e86e1466aab22190904d6561b625945a1d4c97f0
Claudio-Emmolo <Claudio-Emmolo@users.noreply.github.com>
2023-10-26 15:16:32 +0000

    Fix styling

--- src/Shortcodes/TmdbShortcode.php
+++ src/Shortcodes/TmdbShortcode.php
@@ -56,10 +56,11 @@ class TmdbShortcode
             ->acceptJson()
             ->get("https://api.themoviedb.org/$tmdbApiVersion/$type/$id?language=$tmdbLanguage");
 
-        if ($response->failed()){
-            if(config('app.debug')) {
+        if ($response->failed()) {
+            if (config('app.debug')) {
                 throw new \Exception('Error while fetching data from TMDB API');
             }
+
             return null;
         }
 
