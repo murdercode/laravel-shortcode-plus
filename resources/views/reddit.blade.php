2de2fe08436e774dbe1412339159cc7f40674baf
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-10-30 10:56:55 +0100

    Remove Reddit script and Facebook SDK script

--- resources/views/reddit.blade.php
+++ resources/views/reddit.blade.php
@@ -1,4 +1,4 @@
 	<blockquote class="reddit-embed-bq" data-embed-height="500">
 		<a href="{!! $url ?? '#' !!}"></a>
 	</blockquote>
-<script async="" src="https://embed.reddit.com/widgets.js" charset="UTF-8"></script>
+{{--<script async="" src="https://embed.reddit.com/widgets.js" charset="UTF-8"></script>--}}
