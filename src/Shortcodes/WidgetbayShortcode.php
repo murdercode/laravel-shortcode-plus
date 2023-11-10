ff15f2262a5adb822b9c82aef2f6e48df1368ae1
Claudio-Emmolo <Claudio-Emmolo@users.noreply.github.com>
2023-11-10 16:38:26 +0000

    Fix styling

--- src/Shortcodes/WidgetbayShortcode.php
+++ src/Shortcodes/WidgetbayShortcode.php
@@ -7,17 +7,16 @@ class WidgetbayShortcode
     public function register($shortcode)
     {
 
-        if (!$shortcode->id || !is_numeric($shortcode->id) && !$shortcode->link || $shortcode->id && $shortcode->link) {
+        if (! $shortcode->id || ! is_numeric($shortcode->id) && ! $shortcode->link || $shortcode->id && $shortcode->link) {
             return 'No Widgetbay parameter id or link defined';
         }
 
         $id = $shortcode->id ? (number_format($shortcode->id)) : null;
         $link = $shortcode->link;
 
-//        if (!$id && !$link || $id && $link) {
-//            return 'No Widgetbay parameter id or link defined';
-//        }
-
+        //        if (!$id && !$link || $id && $link) {
+        //            return 'No Widgetbay parameter id or link defined';
+        //        }
 
         return view('shortcode-plus::widgetbay', compact('id', 'link'))->render();
     }
