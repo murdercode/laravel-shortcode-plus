fc2b1c00ce309b4181b684019aecba07a656117f
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-11-13 09:58:22 +0100

    Fix conditional statement in WidgetbayShortcode.php to properly check for valid parameters.

--- src/Shortcodes/WidgetbayShortcode.php
+++ src/Shortcodes/WidgetbayShortcode.php
@@ -7,16 +7,13 @@ class WidgetbayShortcode
     public function register($shortcode)
     {
 
-        if (! $shortcode->id || ! is_numeric($shortcode->id) && ! $shortcode->link || $shortcode->id && $shortcode->link) {
+        if (! $shortcode->id && ! $shortcode->link || $shortcode->id && $shortcode->link || $shortcode->id && ! is_numeric($shortcode->id)) {
             return 'No Widgetbay parameter id or link defined';
         }
 
         $id = $shortcode->id ? (number_format($shortcode->id)) : null;
         $link = $shortcode->link;
 
-        //        if (!$id && !$link || $id && $link) {
-        //            return 'No Widgetbay parameter id or link defined';
-        //        }
 
         return view('shortcode-plus::widgetbay', compact('id', 'link'))->render();
     }
