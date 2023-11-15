d9fbc9413bbf65cb2147b6325624deb3c97a6880
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-11-15 15:24:09 +0100

    Add conditional statement to include oembed if widgetbayLink is not set.

--- resources/views/widgetbay.blade.php
+++ resources/views/widgetbay.blade.php
@@ -1,2 +1,8 @@
-<iframe src="{{ $widgetbayLink }}" allowfullscreen="true" scrolling="no"
-        class="shortcode_widgetbay _iub_cs_activate" frameborder="0" height="420" loading="lazy" style="width:100%;overflow-y:hidden;"></iframe>
+@if(isset($widgetbayLink))
+    <iframe src="{{ $widgetbayLink }}" allowfullscreen="true" scrolling="no"
+            class="shortcode_widgetbay _iub_cs_activate" frameborder="0" height="420" loading="lazy" style="width:100%;overflow-y:hidden;"></iframe>
+@else
+    {!! $oembed->css !!}
+    {!! $oembed->html !!}
+@endif
+
