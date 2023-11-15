e201b8a34d399067d8153e540a241e3415ee2e9b
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-11-15 15:25:14 +0100

    Add conditional statement to include iframe if type is "iframe".

--- src/Shortcodes/WidgetbayShortcode.php
+++ src/Shortcodes/WidgetbayShortcode.php
@@ -18,10 +18,34 @@ class WidgetbayShortcode
             $widgetbayLink = 'https://widgetbay.3labs.it/widgetbox?link='.$shortcode->link;
         }
 
-        if (empty($widgetbayLink)) {
-            return 'No Widgetbay parameter id or link defined';
+        if($shortcode->type === 'iframe'){
+            if (empty($widgetbayLink)) {
+                return 'No Widgetbay parameter id or link defined';
+            }
+
+            return view('shortcode-plus::widgetbay', compact('widgetbayLink'))->render();
+        } else {
+            $oembed = self::getOembed($shortcode->link ?? null, $shortcode->id ?? null);
+
+            return view('shortcode-plus::widgetbay', compact('oembed'))->render();
         }
 
-        return view('shortcode-plus::widgetbay', compact('widgetbayLink'))->render();
+    }
+
+    // Get oEmbed from Widgetbay apibox
+    private static function getOembed(?string $url, ?int $id)
+    {
+        curl_setopt_array($curl = curl_init(), [
+            CURLOPT_URL => 'http://widgetbay.test/apibox' . ($url != null ? '?link=' . $url : '/' . $id),
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HTTPHEADER => [
+                'Token: ' . config('shortcode-plus.widgetbox-token'),
+                'Content-Type: application/json',
+            ],
+        ]);
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        return json_decode($response) ?? null;
     }
 }
