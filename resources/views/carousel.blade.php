df7049fe24da306af61b555d46f2bf8bd0398507
Stefano Novelli <murdercode@gmail.com>
2023-12-22 10:45:05 +0100

    Carousel, fix dark + contain

--- resources/views/carousel.blade.php
+++ resources/views/carousel.blade.php
@@ -3,10 +3,10 @@
         <ul class="splide__list">
             @foreach (json_decode($images) as $index => $image)
                 <li class="splide__slide">
-                    <span class="splide_pag absolute top-0 right-0 bg-white px-2">Immagine {{ $index + 1 }} di {{ $images->count() }}</span>
+                    <span class="splide_pag absolute top-0 right-0 bg-white/90 dark:bg-black/90 px-2">Immagine {{ $index + 1 }} di {{ $images->count() }}</span>
                     <img src="{{ asset('storage/' . $image->src )}}?width=896"
                          alt="{{ $image->title }}"
-                         class="w-full h-full object-cover glightbox"
+                         class="w-full h-full object-contain glightbox"
                          data-glightbox="{{ addslashes('carousel-' . $image->title) }}"
                          loading="lazy"
                          title="Clicca per vedere l'immagine originale
