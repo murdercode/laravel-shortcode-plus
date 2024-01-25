a922c512648f497ea85ed8b9960cd42de3f15bd4
Claudio-Emmolo <113107618+Claudio-Emmolo@users.noreply.github.com>
2024-01-25 10:04:48 +0100

    Fix index shortcode

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,9 @@
-<section class="not-prose">
+<section class="index not-prose">
     @foreach($index as $item)
         <a href="#{{$item['id']}}"
-        style="margin-left:
-        {{$item['level'] == 3 ? '8px' : ''}}
-        {{$item['level'] == 4 ? '16px' : ''}}; display: block"
+        style="
+        {{$item['level'] == 3 ? 'margin-left: 15px; color: #313131;' : ''}}
+        {{$item['level'] == 4 ? 'margin-left: 30px; color: #484848;' : ''}};"
         >
             {{$item['title']}}
         </a>
--- src/Parsers/Index.php
+++ src/Parsers/Index.php
@@ -6,16 +6,23 @@ class Index
 {
     public static function parse(string $content): string
     {
+        //Get index list and new content with ID
+        [$index, $newContent] = self::extractHeadlines($content);
 
         return preg_replace_callback(
             '/\[index\]/',
-            function ($matches) use($content) {
-                $index = self::extractHeadlines($content);
+            function ($matches) use($index, $newContent) {
                 return view('shortcode-plus::index', compact('index'))->render();
             },
-            $content
+            $newContent
         );
     }
+
+    public static function newDom()
+    {
+        return '<div class="index">Ciao</div>';
+    }
+
     public static function extractHeadlines($content)
     {
         //Get all the headlines from the content
@@ -24,15 +31,21 @@ class Index
         $dom->loadHTML('<?xml encoding="utf-8" ?>'.$content);
         $xpath = new \DOMXPath($dom);
         $headings = $xpath->query('//h2 | //h3 | //h4');
+
         foreach ($headings as $heading) {
+
+            //Create and add id to the heading
+            $heading->setAttribute('id', str_replace(' ', '-', strtolower($heading->textContent)));
+            $newContent = $dom->saveHTML();
+
             $headlines[] = [
-                'id' => '',
+                'id' => $heading->getAttribute('id'),
                 'title' => $heading->textContent,
                 'level' => (int) substr($heading->tagName, 1),
             ];
         }
 
-        return $headlines;
+        return [$headlines, $newContent];
     }
 
 }
