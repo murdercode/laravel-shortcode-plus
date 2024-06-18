aaf69ad832c0eb2add07ddb30db8e9f448e95479
Stefano Novelli <murdercode@gmail.com>
2024-06-18 18:39:03 +0200

    Refactor name tests on parsed links

--- tests/ParsedLinks.php
+++ tests/ParsedLinks.php
@@ -2,30 +2,28 @@
 
 use Murdercode\LaravelShortcodePlus\LaravelShortcodePlus;
 
-//metodo: forceRel()
-// === SPONSORED (regex) ===
-it('can parse a Sponsored link (amazon.it)', function () {
+it('can parse a sponsored link (amazon.it)', function () {
     $html = '<a href="https://www.amazon.it/WYBOT-Cordless-aspirapolvere-Automatico-interrate/dp/B0B27WRG45">Amazon Link</a>';
     $parsedHtml = LaravelShortcodePlus::source($html)->forceRel()->parseAll();
     expect($parsedHtml)
         ->toContain('<a href="https://www.amazon.it/WYBOT-Cordless-aspirapolvere-Automatico-interrate/dp/B0B27WRG45" rel="sponsored">Amazon Link</a>');
 });
 
-it('can parse a Sponsored link (amazon.com)', function () {
+it('can parse a sponsored link (amazon.com)', function () {
     $html = '<a href="https://www.amazon.com/DJI-Mini-RC-Lightweight-Intelligent/dp/B0BL3NZT5D">Amazon.com Link</a>';
     $parsedHtml = LaravelShortcodePlus::source($html)->forceRel()->parseAll();
     expect($parsedHtml)
         ->toContain('<a href="https://www.amazon.com/DJI-Mini-RC-Lightweight-Intelligent/dp/B0BL3NZT5D" rel="sponsored">Amazon.com Link</a>');
 });
 
-it('can parse a Sponsored link (ebay.it)', function () {
+it('can parse a sponsored link (ebay.it)', function () {
     $html = '<a href="https://www.ebay.it/itm/256462052414?_trkparms=5373:0%7C5374:Featured">Ebay Link</a>';
     $parsedHtml = LaravelShortcodePlus::source($html)->forceRel()->parseAll();
     expect($parsedHtml)
         ->toContain('<a href="https://www.ebay.it/itm/256462052414?_trkparms=5373:0%7C5374:Featured" rel="sponsored">Ebay Link</a>');
 });
 
-it('can parse a Sponsored link (ebay.com)', function () {
+it('can parse a sponsored link (ebay.com)', function () {
     $html = '<a href="https://www.ebay.com/itm/364847373184?_trkparms=5373:0%7C5374:Featured">Ebay.com Link</a>';
     $parsedHtml = LaravelShortcodePlus::source($html)->forceRel()->parseAll();
     expect($parsedHtml)
@@ -33,7 +31,7 @@ it('can parse a Sponsored link (ebay.com)', function () {
 });
 
 // === DOFOLLOW LINKS ===
-it('can parse a DoFollow link', function () {
+it('can parse a doFollow link', function () {
     $html = '<a href="https://forum.tomshw.it/forums/presentati-alla-community.138/">Forum Link</a>';
     $parsedHtml = LaravelShortcodePlus::source($html)->forceRel()->parseAll();
     expect($parsedHtml)
@@ -41,14 +39,14 @@ it('can parse a DoFollow link', function () {
 });
 
 // === NOFOLLOW LINKS ===
-it('can parse a NoFollow link', function () {
+it('can parse a nofollow link', function () {
     $html = '<a href="https://www.youtube.com/watch?v=L9G60Mlig2Q&t=4s">Youtube Link</a>';
     $parsedHtml = LaravelShortcodePlus::source($html)->forceRel()->parseAll();
     expect($parsedHtml)
         ->toContain('<a href="https://www.youtube.com/watch?v=L9G60Mlig2Q&t=4s" rel="nofollow">Youtube Link</a>');
 });
 
-it('is not possible parse whether the connection already has a rel (sponsored)', function () {
+it('cannot parse rel already sponsored', function () {
     $html = '<a href="https://www.youtube.com/watch?v=L9G60Mlig2Q&t=4s" rel="sponsored">Youtube Link</a>';
     $parsedHtml = LaravelShortcodePlus::source($html)->forceRel()->parseAll();
     expect($parsedHtml)
@@ -56,14 +54,14 @@ it('is not possible parse whether the connection already has a rel (sponsored)',
 });
 
 // === LINK WITH DEFAULT REL ===
-it('is not possible parse whether the connection already has a rel (youtube with dofollow)', function () {
+it('cannot parse rel already dofollow', function () {
     $html = '<a href="https://www.youtube.com/watch?v=L9G60Mlig2Q&t=4s" rel="dofollow">Youtube Link</a>';
     $parsedHtml = LaravelShortcodePlus::source($html)->forceRel()->parseAll();
     expect($parsedHtml)
         ->toContain('<a href="https://www.youtube.com/watch?v=L9G60Mlig2Q&t=4s" rel="dofollow">Youtube Link</a>');
 });
 
-it('is not possible parse whether the connection already has a rel (amazon with nofollow)', function () {
+it('cannot parse rel already nofollow', function () {
     $html = '<a href="https://www.amazon.it/WYBOT-Cordless-aspirapolvere-Automatico-interrate/dp/B0B27WRG45" rel="nofollow">Amazon Link</a>';
     $parsedHtml = LaravelShortcodePlus::source($html)->forceRel()->parseAll();
     expect($parsedHtml)
