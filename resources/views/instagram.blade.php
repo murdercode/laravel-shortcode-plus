86ad85b9d3505883c30dc06884b0e7d3fa282d2b
Stefano Novelli <stefano.novelli@tomshw.it>
2024-06-20 19:08:47 +0200

    Update instagram.blade.php

--- resources/views/instagram.blade.php
+++ resources/views/instagram.blade.php
@@ -1,1 +1,1 @@
-<iframe src="{{$embed_url}}" class="mx-auto" width="600px" height="600px" frameborder="0" scrolling="no" allowtransparency="true"></iframe>
+<iframe src="{{$embed_url}}" class="mx-auto" width="600px" height="600px" frameborder="0" scrolling="no" allowtransparency="true" loading="lazy"></iframe>
