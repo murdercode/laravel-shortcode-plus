c34a64b07e32bcbe970915bf75578389fa72f86d
Claudio-Emmolo <113107618+Claudio-Emmolo@users.noreply.github.com>
2024-07-10 11:22:36 +0200

    Fix widgetbay heights

--- src/Shortcodes/WidgetbayShortcode.php
+++ src/Shortcodes/WidgetbayShortcode.php
@@ -8,24 +8,44 @@ class WidgetbayShortcode
     {
 
         $widgetbayLink = '';
-
+        $height = null;
         if ($shortcode->id) {
-            $widgetbayLink = 'https://widgetbay.3labs.it/widgetbox/'.$shortcode->id;
+            $widgetbayLink = 'https://widgetbay.test/widgetbox/' . $shortcode->id;
         }
 
         if ($shortcode->link) {
             $shortcode->link = str_replace('&', '%26', $shortcode->link);
-            $widgetbayLink = 'https://widgetbay.3labs.it/widgetbox?link='.$shortcode->link;
+            $height = $this->calculateIframeHeight($shortcode->link);
+            $widgetbayLink = 'https://widgetbay.test/widgetbox?link=' . $shortcode->link;
         }
 
         if ($shortcode->title) {
-            $widgetbayLink .= '&title='.$shortcode->title;
+            $widgetbayLink .= '&title=' . $shortcode->title;
         }
 
         if (empty($widgetbayLink)) {
             return 'No Widgetbay parameter id or link defined';
         }
 
-        return view('shortcode-plus::widgetbay', compact('widgetbayLink'))->render();
+        return view('shortcode-plus::widgetbay', compact('widgetbayLink', 'height'))->render();
+    }
+
+    protected function calculateIframeHeight($products)
+    {
+        $products = explode(',', $products);
+        $count = count($products);
+
+
+        if ($count > 1) {
+            $defaultHeightDesktop = 92;
+            $defaultHeightMobile = 142;
+
+            return [
+                "desktop" => $defaultHeightDesktop * $count,
+                "mobile" => $defaultHeightMobile * $count
+            ];
+        }
+
+        return null;
     }
 }
