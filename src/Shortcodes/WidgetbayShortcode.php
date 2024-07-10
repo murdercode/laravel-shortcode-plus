f48eb38a491d4cd456fbc7c2a5f3074530ed5a44
Claudio-Emmolo <Claudio-Emmolo@users.noreply.github.com>
2024-07-10 12:13:13 +0000

    Fix styling

--- src/Shortcodes/WidgetbayShortcode.php
+++ src/Shortcodes/WidgetbayShortcode.php
@@ -10,17 +10,17 @@ class WidgetbayShortcode
         $widgetbayLink = '';
         $heightListClass = null;
         if ($shortcode->id) {
-            $widgetbayLink = 'https://widgetbay.test/widgetbox/' . $shortcode->id;
+            $widgetbayLink = 'https://widgetbay.test/widgetbox/'.$shortcode->id;
         }
 
         if ($shortcode->link) {
             $shortcode->link = str_replace('&', '%26', $shortcode->link);
             $heightListClass = $this->calculateIframeHeight($shortcode->link);
-            $widgetbayLink = 'https://widgetbay.test/widgetbox?link=' . $shortcode->link;
+            $widgetbayLink = 'https://widgetbay.test/widgetbox?link='.$shortcode->link;
         }
 
         if ($shortcode->title) {
-            $widgetbayLink .= '&title=' . $shortcode->title;
+            $widgetbayLink .= '&title='.$shortcode->title;
         }
 
         if (empty($widgetbayLink)) {
@@ -35,9 +35,8 @@ class WidgetbayShortcode
         $products = explode(',', $products);
         $count = count($products);
 
-
         if ($count > 1) {
-            return 'shortcode_widgetbay_list_' . $count;
+            return 'shortcode_widgetbay_list_'.$count;
         }
 
         return null;
