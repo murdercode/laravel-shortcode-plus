8b262ae7671d9190f159dd5803e72ff836733db7
Claudio-Emmolo <Claudio-Emmolo@users.noreply.github.com>
2024-11-25 10:50:28 +0000

    Fix styling

--- src/Shortcodes/WidgetbayShortcode.php
+++ src/Shortcodes/WidgetbayShortcode.php
@@ -6,30 +6,30 @@ class WidgetbayShortcode
 {
     public function register($shortcode)
     {
-        $endpoint = config('shortcode-plus.widgetbay.endpoint') . '/widgetbox';
+        $endpoint = config('shortcode-plus.widgetbay.endpoint').'/widgetbox';
 
         $widgetbayLink = '';
         $heightListClass = null;
         if ($shortcode->id) {
-            $widgetbayLink = $endpoint . '/' . $shortcode->id;
+            $widgetbayLink = $endpoint.'/'.$shortcode->id;
         }
 
         if ($shortcode->link) {
             $shortcode->link = str_replace('&', '%26', $shortcode->link);
             $heightListClass = $this->calculateIframeHeight($shortcode->link, $shortcode->layout);
-            $widgetbayLink = $endpoint . '?link=' . $shortcode->link;
+            $widgetbayLink = $endpoint.'?link='.$shortcode->link;
         }
 
         if ($shortcode->title) {
-            $widgetbayLink .= '&title=' . $shortcode->title;
+            $widgetbayLink .= '&title='.$shortcode->title;
         }
 
         if ($shortcode->forcelink) {
-            $widgetbayLink .= '&forceLink=' . $shortcode->forcelink;
+            $widgetbayLink .= '&forceLink='.$shortcode->forcelink;
         }
 
         if ($shortcode->layout) {
-            $widgetbayLink .= '&layout=' . $shortcode->layout;
+            $widgetbayLink .= '&layout='.$shortcode->layout;
         }
 
         if (empty($widgetbayLink)) {
@@ -41,18 +41,17 @@ class WidgetbayShortcode
 
     protected function calculateIframeHeight($products, $layout = null)
     {
-        $endpoint = config('shortcode-plus.widgetbay.endpoint') . '/api/widgetbox-count-available-products?link=' . $products;
-
+        $endpoint = config('shortcode-plus.widgetbay.endpoint').'/api/widgetbox-count-available-products?link='.$products;
 
         //create HTTP request
-        $request = new \GuzzleHttp\Client();
+        $request = new \GuzzleHttp\Client;
         $response = $request->get($endpoint);
         $count = json_decode($response->getBody()->getContents(), true);
 
         $prefix = $layout === 'hero' ? 'shortcode_widgetbay_list_hero_' : 'shortcode_widgetbay_list_';
 
         if ($count > 1) {
-            return $prefix . $count;
+            return $prefix.$count;
         }
 
         return null;
