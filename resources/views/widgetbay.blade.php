3c715dae025568161e124f039016e813e95504cc
Stefano Novelli <murdercode@gmail.com>
2024-12-10 09:51:04 +0100

    Remove stretched from link (due to multiproduct)

--- resources/views/widgetbay.blade.php
+++ resources/views/widgetbay.blade.php
@@ -1,10 +1,7 @@
-<div class="relative">
-    <iframe src="{{ $widgetbayLink }}" allow="fullscreen" scrolling="no"
-            class="{{ $heightListClass ?? 'shortcode_widgetbay' }} _iub_cs_activate"
-            frameborder="0" loading="lazy"
-            title="Offerta di Widgetbay"
-            style="width:100%;overflow-y:hidden;
+<iframe src="{{ $widgetbayLink }}" allow="fullscreen" scrolling="no"
+        class="{{ $heightListClass ?? 'shortcode_widgetbay' }} _iub_cs_activate"
+        frameborder="0" loading="lazy"
+        title="Offerta di Widgetbay"
+        style="width:100%;overflow-y:hidden;
             ">
-    </iframe>
-    <a href="{{ $widgetbayLink }}" target="_blank" rel="sponsored nofollow" class="widgetbay_box stretched-link"></a>
-</div>
+</iframe>
