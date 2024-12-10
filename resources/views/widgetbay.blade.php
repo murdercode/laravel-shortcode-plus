a8d754ffa1c4201887b228d56beb67a55685a1cb
Stefano Novelli <murdercode@gmail.com>
2024-12-10 09:38:40 +0100

    Add widgetbay stretched link for GTM tracking

--- resources/views/widgetbay.blade.php
+++ resources/views/widgetbay.blade.php
@@ -1,6 +1,10 @@
-<iframe src="{{ $widgetbayLink }}" allowfullscreen="true" scrolling="no"
-        class="{{ $heightListClass ?? 'shortcode_widgetbay' }} _iub_cs_activate"
-        frameborder="0" loading="lazy"
-        style="width:100%;overflow-y:hidden;
-        ">
-</iframe>
+<div class="relative">
+    <iframe src="{{ $widgetbayLink }}" allow="fullscreen" scrolling="no"
+            class="{{ $heightListClass ?? 'shortcode_widgetbay' }} _iub_cs_activate"
+            frameborder="0" loading="lazy"
+            title="Offerta di Widgetbay"
+            style="width:100%;overflow-y:hidden;
+            ">
+    </iframe>
+    <a href="{{ $widgetbayLink }}" target="_blank" rel="sponsored nofollow" class="widgetbay_box stretched-link"></a>
+</div>
