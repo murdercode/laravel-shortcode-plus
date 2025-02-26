339aa5fad0e1fbb29f57ee2ac782426c006a08b4
Stefano Novelli <murdercode@gmail.com>
2025-02-26 14:11:46 +0100

    Remove nocookie text from Bluesky view for cleaner output

--- resources/views/bluesky.blade.php
+++ resources/views/bluesky.blade.php
@@ -2,9 +2,6 @@
     <div class="flex justify-center bsky-card">
         {!! $html !!}
     </div>
-    <aside class="shortcode_nocookie">
-        {!! config('shortcode-plus.nocookie.text') !!} - bsky.app
-    </aside>
 @else
     <p>Sorry, no post found.</p>
 @endif
