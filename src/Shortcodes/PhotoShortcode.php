ca51e1ea62cbd62e095369e3aae316c72701f1a7
Claudio-Emmolo <113107618+Claudio-Emmolo@users.noreply.github.com>
2025-05-30 14:07:42 +0200

    Refactor media and gallery shortcodes for improved layout and responsiveness; enhance image handling and styling

--- src/Shortcodes/PhotoShortcode.php
+++ src/Shortcodes/PhotoShortcode.php
@@ -6,11 +6,12 @@ class PhotoShortcode
 {
     public function register($shortcode): string
     {
-        if (! class_exists('\Outl1ne\NovaMediaHub\Models\Media')) {
+        if (!class_exists('\Outl1ne\NovaMediaHub\Models\Media')) {
             return '';
         }
 
         $multipleIds = preg_match('/\d+(,\s*\d+)*/', $shortcode->id, $matches) ? $matches[0] : null;
+        $flexGallery = $shortcode->effect == 'gallery-flex' ? true : false;
 
         if ($multipleIds) {
             $ids = explode(',', $multipleIds);
@@ -23,7 +24,7 @@ class PhotoShortcode
                 });
 
                 foreach ($images as $key => $image) {
-                    $images[$key]['src'] = $image->path.$image->file_name;
+                    $images[$key]['src'] = $image->path . $image->file_name;
                     $images[$key]['title'] = $image['data']['title'][0] ?? null;
                     $images[$key]['alt'] = $image['data']['alt'][0] ?? null;
                 }
@@ -37,7 +38,6 @@ class PhotoShortcode
                     return view('shortcode-plus::juxtapose', compact('images', 'title'))->render();
                 }
 
-                $flexGallery = $shortcode->effect == 'gallery-flex' ? true : false;
                 $imageToDisplay = $flexGallery ? config('shortcode-plus.gallery.flex.imageToDisplay') : config('shortcode-plus.gallery.imageToDisplay');
 
                 return view('shortcode-plus::new-gallery', compact('images', 'title', 'flexGallery', 'imageToDisplay'))->render();
@@ -46,12 +46,12 @@ class PhotoShortcode
 
         // Single image
         $media = \Outl1ne\NovaMediaHub\Models\Media::find($shortcode->id);
-        if (! $media) {
+        if (!$media) {
             return '';
         }
 
-        $path = $media->path.$media->file_name;
-        $align = $shortcode->align ?? null;
+        $path = $media->path . $media->file_name;
+//        $align = $shortcode->align ?? null;
         $link = $shortcode->link ? str_replace("'", '%27', $shortcode->link) : null;
         $shape = $shortcode->shape ?? null;
 
@@ -67,7 +67,7 @@ class PhotoShortcode
         if (is_array($credits)) {
             $credits = $credits[0];
         }
-        $alt = $media->data['alt'] ?? 'Immagine id '.$shortcode->id;
+        $alt = $media->data['alt'] ?? 'Immagine id ' . $shortcode->id;
         if (is_array($alt)) {
             $alt = $alt[0];
         }
@@ -87,7 +87,7 @@ class PhotoShortcode
         $width = $shortcode->width ?? $maxWidth;
         $height = $shortcode->height ?? self::getImageHeight($path, $width);
 
-        return view('shortcode-plus::media', compact('path', 'align', 'maxWidth', 'link', 'shape', 'didascalia', 'credits', 'alt', 'title', 'width', 'height'))->render();
+        return view('shortcode-plus::media', compact('path', 'flexGallery', 'maxWidth', 'link', 'shape', 'didascalia', 'credits', 'alt', 'title', 'width', 'height'))->render();
     }
 
     /**
@@ -95,10 +95,10 @@ class PhotoShortcode
      */
     public static function getImageHeight(string $path, int $width = 0): float|int
     {
-        $localPath = storage_path('app/public/'.$path);
+        $localPath = storage_path('app/public/' . $path);
 
         // Check if file exists
-        if (! file_exists($localPath)) {
+        if (!file_exists($localPath)) {
             return 0;
         }
 
