5ed999baeaceefb166f84744588f4c87ec9f342b
Claudio-Emmolo <113107618+Claudio-Emmolo@users.noreply.github.com>
2025-06-09 08:41:56 +0000

    Fix styling

--- src/Shortcodes/PhotoShortcode.php
+++ src/Shortcodes/PhotoShortcode.php
@@ -24,7 +24,7 @@ class PhotoShortcode
                 });
 
                 foreach ($images as $key => $image) {
-                    $images[$key]['src'] = $image->path . $image->file_name;
+                    $images[$key]['src'] = $image->path.$image->file_name;
                     $images[$key]['title'] = $image['data']['title'][0] ?? null;
                     $images[$key]['alt'] = $image['data']['alt'][0] ?? null;
                 }
@@ -50,12 +50,12 @@ class PhotoShortcode
             return '';
         }
 
-        $path = $media->path . $media->file_name;
+        $path = $media->path.$media->file_name;
         $align = $shortcode->align ?? null;
         $link = $shortcode->link ? str_replace("'", '%27', $shortcode->link) : null;
         $shape = $shortcode->shape ?? null;
         $isSquare = $shape === 'rounded';
-        
+
         $didascalia = $shortcode->didascalia ?? $media->data['caption'] ?? null;
 
         if (is_array($didascalia)) {
@@ -68,7 +68,7 @@ class PhotoShortcode
         if (is_array($credits)) {
             $credits = $credits[0];
         }
-        $alt = $media->data['alt'] ?? 'Immagine id ' . $shortcode->id;
+        $alt = $media->data['alt'] ?? 'Immagine id '.$shortcode->id;
         if (is_array($alt)) {
             $alt = $alt[0];
         }
@@ -96,7 +96,7 @@ class PhotoShortcode
      */
     public static function getImageHeight(string $path, int $width = 0): float|int
     {
-        $localPath = storage_path('app/public/' . $path);
+        $localPath = storage_path('app/public/'.$path);
 
         // Check if file exists
         if (! file_exists($localPath)) {
